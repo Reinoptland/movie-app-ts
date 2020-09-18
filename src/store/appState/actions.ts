fc5d742c1a510239b904f239d302fb30a1d3887c
Reinoptland <reinoptland@gmail.com>
2020-09-18 12:25:43 +0200

    Handle action in reducer

--- src/store/appState/actions.ts
+++ src/store/appState/actions.ts
@@ -1,3 +1,9 @@
 export const APP_LOADING = "APP_LOADING";
 
-export const appLoading = { type: APP_LOADING };
+type TappLoading = {
+  type: "APP_LOADING";
+};
+
+export const appLoading: TappLoading = { type: APP_LOADING };
+
+export type TAppStateAction = TappLoading;
--- src/store/appState/reducer.ts
+++ src/store/appState/reducer.ts
@@ -1,10 +1,17 @@
 import { TAppState } from "../../entities/store";
+import { APP_LOADING, TAppStateAction } from "./actions";
 
 const initialState: TAppState = {
   loading: false,
 };
 // moet een pure function zijn
 // moet altijd een state terug geven
-export default (state: TAppState = initialState, action: any) => {
-  return state;
+export default (state: TAppState = initialState, action: TAppStateAction) => {
+  switch (action.type) {
+    case APP_LOADING:
+      return { loading: true };
+
+    default:
+      return state;
+  }
 };
