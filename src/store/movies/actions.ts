ad79ae3250c684b505962facdf6e1667f7b728a2
Reinoptland <reinoptland@gmail.com>
2020-09-18 15:47:27 +0200

    Add comments

--- src/store/movies/actions.ts
+++ src/store/movies/actions.ts
@@ -17,17 +17,26 @@ export type TMoviesActions = TsearchSuccess;
 
 export const searchMoviesThunk = (searchText: string) => {
   return async (dispatch: (action: any) => void, getState: () => TStore) => {
+    // wat is huidige state?
     const state = getState();
 
+    // hebben we al resultaten voor deze zoekopdracht?
+    // zo ja return (stop! we hebben de data al)
     if (state.movieState.search[searchText] !== undefined) return;
 
+    // zo nee, dan gaan we een request maken, dus de app gaat laden (laat een spinner zien)
     dispatch(appLoading);
 
+    // vertraagt de request 1 seconden (dan kan je die spinner)
+    // puur voor het voorbeeld
     await new Promise((resolve) => setTimeout(resolve, 1000));
 
+    // maak een request naar de api
     const result = await searchMoviesByTitle(searchText);
 
+    // on success, (Reponse: True)
     if (result.Response === "True") {
+      // maak een actie met type & payload
       const action: TsearchSuccess = {
         type: SEARCH_SUCCESS,
         payload: {
@@ -36,9 +45,11 @@ export const searchMoviesThunk = (searchText: string) => {
         },
       };
 
+      // dispatch de actie
       dispatch(action);
     }
 
+    // we zijn klaar met laden, haal de spinner weg
     dispatch(appDoneLoading);
   };
 };
